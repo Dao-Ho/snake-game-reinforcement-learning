7a54da5afd09162cb42afa76d655c7490fb43f41
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-05-01 17:38:17 -0400

    display board properly

--- snake-engine/snake_game.cpp
+++ snake-engine/snake_game.cpp
@@ -1,13 +1,18 @@
 #include "snake/board.h"
+
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 using namespace SnakeEngine;
 
 int main() {
-  Board game_board(21, 24);
+  Board game_board(24, 21);
   while (!game_board.IsGameOver()) {
-    game_board.DisplayBoard();
     std::cout << "\033[2J\033[H";
+    game_board.DisplayBoard();
+    std::cout.flush();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
   return 0;
 }
