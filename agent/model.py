2a7b54d9c2893c74fd4cc66b34b7d8a6d8d4eda7
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-05-06 15:11:48 -0400

    feat: initialize model (#13)

---
+++ agent/model.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DQN(nn.Module): 
+    def __init__(self, input_size: int, output_size: int):
+        super().__init__()
+        self.layer1 = nn.Linear(input_size, 256)
+        self.layer2 = nn.Linear(256, 128)
+        self.layer3 = nn.Linear(128, output_size)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = F.relu(x)
+        x = self.layer2(x)
+        x = F.relu(x)
+        x = self.layer3(x)
+        return x
