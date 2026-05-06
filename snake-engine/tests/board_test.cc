efabbafbb2acb58404002d30c5b03bf2ec202053
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-05-05 21:59:11 -0400

    feat: first pass with test cases passed

--- snake-engine/tests/board_test.cc
+++ snake-engine/tests/board_test.cc
@@ -1,16 +1,24 @@
 #include "snake/board.h"
 #include <gtest/gtest.h>
 
-using namespace snake_engine;
+using namespace SnakeEngine;
 
 TEST(BoardTest, InitialStateNotGameOver) {
   Board board(10, 10);
   EXPECT_FALSE(board.IsGameOver());
 }
 
-
 TEST(BoardTest, InitialStateZeroScore) {
   Board board(10, 10);
-  EXPECT_FALSE(board.GetScore(), 0);
+  EXPECT_EQ(board.GetScore(), 0);
 }
 
+TEST(BoardTest, OutofBoundsCorrect) {
+  const int size = 5;
+  const int moves_until_out_of_bounds = (size / 2) + 1;
+  Board board(size, size);
+  for (int i = 0; i < moves_until_out_of_bounds; ++i) {
+    board.ApplyMove(Move::kUp);
+  }
+  EXPECT_TRUE(board.IsGameOver());
+}
