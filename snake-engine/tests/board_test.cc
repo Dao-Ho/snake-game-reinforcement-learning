beaa66e19c45f975c06ca1e3bcd371d3875764f2
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-05-05 22:02:03 -0400

    feat: more tests

--- snake-engine/tests/board_test.cc
+++ snake-engine/tests/board_test.cc
@@ -1,5 +1,8 @@
 #include "snake/board.h"
+
 #include <gtest/gtest.h>
+
+#include <algorithm>
 
 using namespace SnakeEngine;
 
@@ -13,12 +16,72 @@ TEST(BoardTest, InitialStateZeroScore) {
   EXPECT_EQ(board.GetScore(), 0);
 }
 
-TEST(BoardTest, OutofBoundsCorrect) {
-  const int size = 5;
-  const int moves_until_out_of_bounds = (size / 2) + 1;
-  Board board(size, size);
-  for (int i = 0; i < moves_until_out_of_bounds; ++i) {
-    board.ApplyMove(Move::kUp);
-  }
+TEST(BoardTest, OutOfBoundsTopWall) {
+  Board board(10, 10);
+  for (int i = 0; i < 6; ++i) (void)board.ApplyMove(Move::kUp);
   EXPECT_TRUE(board.IsGameOver());
+}
+
+TEST(BoardTest, OutOfBoundsBottomWall) {
+  Board board(10, 10);
+  for (int i = 0; i < 6; ++i) (void)board.ApplyMove(Move::kDown);
+  EXPECT_TRUE(board.IsGameOver());
+}
+
+TEST(BoardTest, OutOfBoundsLeftWall) {
+  Board board(10, 10);
+  for (int i = 0; i < 6; ++i) (void)board.ApplyMove(Move::kLeft);
+  EXPECT_TRUE(board.IsGameOver());
+}
+
+TEST(BoardTest, OutOfBoundsRightWall) {
+  Board board(10, 10);
+  for (int i = 0; i < 6; ++i) (void)board.ApplyMove(Move::kRight);
+  EXPECT_TRUE(board.IsGameOver());
+}
+
+TEST(BoardTest, ReverseDirectionContinuesForward) {
+  Board board(10, 10);
+  ASSERT_TRUE(board.ApplyMove(Move::kDown).ok());
+  EXPECT_FALSE(board.IsGameOver());
+}
+
+TEST(BoardTest, EatingAppleIncrementsScore) {
+  Board board(10, 10);
+  int initial_score = board.GetScore();
+  int moves = 0;
+  while (board.GetScore() == initial_score && !board.IsGameOver() &&
+         moves < 200) {
+    (void)board.ApplyMove(Move::kUp);
+    ++moves;
+  }
+  if (!board.IsGameOver()) {
+    EXPECT_GT(board.GetScore(), initial_score);
+  }
+}
+
+TEST(BoardTest, InitialGridHasExactlyOneApple) {
+  Board board(10, 10);
+  const auto& grid = board.GetGrid();
+  int apple_count = std::count(grid.begin(), grid.end(), Cell::kApple);
+  EXPECT_EQ(apple_count, 1);
+}
+
+TEST(BoardTest, InitialGridHasExactlyOneSnakeHead) {
+  Board board(10, 10);
+  const auto& grid = board.GetGrid();
+  int head_count = std::count(grid.begin(), grid.end(), Cell::kSnakeHead);
+  EXPECT_EQ(head_count, 1);
+}
+
+TEST(BoardTest, AppleCountRemainsOneAfterEating) {
+  Board board(10, 10);
+  for (int i = 0; i < 50 && !board.IsGameOver(); ++i) {
+    (void)board.ApplyMove(Move::kUp);
+  }
+  if (!board.IsGameOver()) {
+    const auto& grid = board.GetGrid();
+    int apple_count = std::count(grid.begin(), grid.end(), Cell::kApple);
+    EXPECT_EQ(apple_count, 1);
+  }
 }
