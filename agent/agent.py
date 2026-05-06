cd491fb025ecc56e1dbaaa54cb93bcbe2faa208d
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-05-06 17:25:26 -0400

    first pass at select action

--- agent/agent.py
+++ agent/agent.py
@@ -1,6 +1,8 @@
 from model import DQN
 from replay_buffer import ReplayBuffer
 import torch.optim as optim
+import torch
+import random
 
 class SnakeGameAgent():
     def __init__(self):
@@ -10,5 +12,13 @@ class SnakeGameAgent():
         self.epsilon = 1.0
         self.epsilon_decay = 0.995
         self.epsilon_min = 0.01
-        
+
+    def select_action(self, state):
+        with torch.no_grad():
+            if random.random() < self.epsilon:
+                # explore a random move that may not be optimal
+                return random.randint(0, 3)
+            else:
+                q_values = self.model(state)
+                return torch.argmax(q_values).item()
     
