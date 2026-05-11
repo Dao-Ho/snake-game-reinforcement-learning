c8e887b32a920313747fd53a1dfe610949e31fef
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2026-05-10 20:49:07 -0400

    feat: increase size of replay buffer during training (#19)

--- agent/agent.py
+++ agent/agent.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 class SnakeGameAgent():
     def __init__(self):
         self.model = DQN(input_size=24*21, output_size=4)
-        self.replay_buffer = ReplayBuffer(256)
+        self.replay_buffer = ReplayBuffer(10000)
         self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
         self.epsilon = 1.0
         self.epsilon_decay = 0.995
